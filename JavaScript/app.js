7700ce80116a165492fe55c7c8eb2355ae16698e
AnasAGc <civil.anas1000@gmail.com>
2021-03-25 15:47:45 +0200

    We just added some JavaScript

---
+++ JavaScript/app.js
@@ -0,0 +1,17 @@
+//Welcome to my Code 
+alert("Welcome to my Humble site");
+
+console.log("HI FROE JAVASCRIPT FILE")
+var A1= prompt( "Would you like to buy from us","yes or no");
+var A2= prompt( "Enter your Name");
+
+document.write("we happy to have you here " + A2)
+
+if(A1==='yes'){
+    alert("We are happy to give you Discount code : Anas0000");
+;
+
+}else{
+    document.write('<img src="https://www.jcrew.com/s7-img-facade/85231_WT0002?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=850&hei=850"')
+
+}
